a6b201287bbc5fd18f709122d9c24bf1bdf452a1
Carlos Mendible <carlos.mendible.porras@repsol.com>
2019-11-18 19:11:17 +0100

    Updated Script to add secret list permission to the keyvault policy

--- src/SetupPodIdentityKeyVaultIntegration.ps1
+++ src/SetupPodIdentityKeyVaultIntegration.ps1
@@ -1,19 +1,19 @@
 param(
-    [string]
-    [Parameter(Mandatory = $true)]
-    $resourceGroupName,
-    [string]
-    [Parameter(Mandatory = $true)]
-    $identityName,
-    [string]
-    [Parameter(Mandatory = $true)]
-    $identitySelector,
-    [string]
-    [Parameter(Mandatory = $true)]
-    $aksName,
-    [string]
-    [Parameter(Mandatory = $true)]
-    $keyVaultName 
+  [string]
+  [Parameter(Mandatory = $true)]
+  $resourceGroupName,
+  [string]
+  [Parameter(Mandatory = $true)]
+  $identityName,
+  [string]
+  [Parameter(Mandatory = $true)]
+  $identitySelector,
+  [string]
+  [Parameter(Mandatory = $true)]
+  $aksName,
+  [string]
+  [Parameter(Mandatory = $true)]
+  $keyVaultName 
 )
 
 # Get the current subscription
@@ -21,32 +21,32 @@ $subscriptionId = (az account show | ConvertFrom-Json).id
 
 # Get aks so we can extract it's Service Princpal later
 $aks = az aks show `
-    -g $resourceGroupName `
-    -n $aksName | ConvertFrom-Json
+  -g $resourceGroupName `
+  -n $aksName | ConvertFrom-Json
 
 # Create Managed Identity
 $identity = az identity create `
-    -g $resourceGroupName `
-    -n $identityName `
-    -o json | ConvertFrom-Json
+  -g $resourceGroupName `
+  -n $identityName `
+  -o json | ConvertFrom-Json
 
 # Assign the Reader role to the Managed Identity
 az role assignment create `
-    --role "Reader" `
-    --assignee $identity.principalId `
-    --scope /subscriptions/$subscriptionId/resourcegroups/$resourceGroupName
+  --role "Reader" `
+  --assignee $identity.principalId `
+  --scope /subscriptions/$subscriptionId/resourcegroups/$resourceGroupName
 
 # Assign the Managed Identity Operator role to the AKS Service Principal
 az role assignment create `
-    --role "Managed Identity Operator" `
-    --assignee $aks.servicePrincipalProfile.clientId `
-    --scope $identity.id
+  --role "Managed Identity Operator" `
+  --assignee $aks.servicePrincipalProfile.clientId `
+  --scope $identity.id
 
 # Add policy to the Key Vault so the Managed Identity can read secrets
 az keyvault set-policy `
-    --name $keyVaultName `
-    --spn $identity.clientId `
-    --secret-permissions get
+  --name $keyVaultName `
+  --spn $identity.clientId `
+  --secret-permissions get list
 
 # Enable AAD Pod Identity on AKS
 kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master/deploy/infra/deployment-rbac.yaml
