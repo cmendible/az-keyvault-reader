cb4b19dde6948c2bed840ef31ea8bf05dcd416e5
cmendible <cmendible@gmail.com>
2019-12-09 22:25:27 +0100

    fixed script

--- src/sidecar-injector/tls/deploy_key_pair.sh
+++ src/sidecar-injector/tls/deploy_key_pair.sh
@@ -1,6 +1,6 @@
-./cert.sh \
+./create_key_pair.sh \
     --service az-keyvault-reader-injector-service \
     --secret az-keyvault-reader-injector \
     --namespace kube-system
     
-./ca_bundle.sh
+./patch_ca_bundle.sh
